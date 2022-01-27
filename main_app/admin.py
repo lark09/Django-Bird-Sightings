94d992476df941eb865b34ada0ac5882d965c257
Alondra Maldonado <alondra.mal96@gmail.com>
2022-01-26 21:30:11 -0800

    added sightings and functionality

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Bird
+from .models import Bird, Sighting
 
 # Register your models here.
 admin.site.register(Bird)
+admin.site.register(Sighting)
